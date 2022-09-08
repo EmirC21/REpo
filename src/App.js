7f9137c29811c6c14ad1c48ed4e11a181bf3f9ec
EmirC21 <emir2932@gmail.com>
2022-09-09 01:21:56 +0300

    resim eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
      <h1>BEN EGLENCEME BAKARIM</h1>
+     <Header/>
     </div>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './header.css'
+ class header extends Component {
+  render() {
+    return (
+      <div>
+        <img src="alpi.jpg" alt="" className='resim'/>
+      </div>
+    )
+  }
+}
+export default header
