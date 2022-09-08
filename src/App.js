bde616392982468bfc14745524cf2c9fe1e0fa95
EmirC21 <emir2932@gmail.com>
2022-09-09 00:08:57 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>emir baba hababm</h1>
+    </div>
+  );
+}
+
+export default App;
